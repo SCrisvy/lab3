9ec9b262812d6e5570a270acaaa3de66fb15024e
shane Ricafort <sgricafort@student.apc.edu.ph>
2023-03-01 14:05:40 +0800

    m

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -7,7 +7,8 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
 
-	protected $table = 'news';
+	protected $table = 'sgricafort_news';
+
 
     protected $allowedFields = ['title', 'slug', 'body'];
 	

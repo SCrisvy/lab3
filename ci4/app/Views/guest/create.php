618b1f695c7ff23dee5c77c066b52220f0bcd7fe
Shane Ricafort <119278144+SCrisvy@users.noreply.github.com>
2023-03-03 20:19:07 +0800

    Update create.php

--- ci4/app/Views/guest/create.php
+++ ci4/app/Views/guest/create.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 }
 
 // sql to create table
-$sql = "CREATE TABLE MyGuests (
+$sql = "CREATE TABLE sgricafort_myguests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 email VARCHAR(50),
@@ -23,7 +23,7 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
+  echo "Table created successfully";
 } else {
   echo "Error creating table: " . $conn->error;
 }

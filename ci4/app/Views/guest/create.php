c7da3689d84972cfe2739c4c1c5cddeceb194f3a
Shane Ricafort <119278144+SCrisvy@users.noreply.github.com>
2023-03-03 20:40:00 +0800

    Update create.php

--- ci4/app/Views/guest/create.php
+++ ci4/app/Views/guest/create.php
@@ -11,23 +11,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE sgricafort_myguests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-website VARCHAR(30) NOT NULL,
-comment TEXT(150) NOT NULL,
-gender VARCHAR(10),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+$sql = "INSERT INTO MyGuests (name, email, website, comment, gender)
+VALUES ('Shane Ricafort', 'sgricafort@student.apc.edu.ph', 'http://apcwebprog.csf.ph/~sgricafort/lab3/ci4/public/index', 'mmmmmmmmmmmmmm', 'Female')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table created successfully";
+  echo "New record created successfully";
 } else {
-  echo "Error creating table: " . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 ?>
-   
